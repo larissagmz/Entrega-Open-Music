baa6b88e979e5371e0d29c0dcb5868ba37f6ef9b
larissagmz <larissagimenes.tinfem2023@gmail.com>
2024-10-10 11:44:59 -0300

    commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,70 +24,21 @@ function renderHeader() {
 
 function renderMain(list) {
     let main = document.createElement("main");
+    main.className = "main";
     main.className = "container";
     document.body.insertBefore(main, body.children[1]);
-    let div = renderFilterMusic(list, categories);
-    main.append(div);
+    renderFilterMusic(list, categories);
+    renderMusic(list);
 }
 
-// function renderFilterMusic() {
-//     let maxPrice = products.reduce(
-//         (max, product) => Math.max(max, product.price),
-//         0
-//     );
-//     let minPrice = products.reduce(
-//         (min, product) => Math.min(min, product.price),
-//         Infinity
-//     );
-
-//     let div = document.createElement("div");
-//     let divTitleFilter = document.createElement("div");
-//     let h2 = document.createElement("h2");
-//     let divButtonsFilter = document.createElement("div");
-//     let divDefinePrice = document.createElement("div");
-//     let textDefinePrice = document.createElement("p");
-//     let valueInput = document.createElement("p");
-//     let output = document.createElement("output");
-//     let input = document.createElement("input");
-
-//     div.append(divTitleFilter, divDefinePrice, input);
-//     divTitleFilter.append(h2, divButtonsFilter);
-//     divDefinePrice.append(textDefinePrice, valueInput);
-//     valueInput.append(output);
-
-//     div.className = "div-all-filter";
-//     divTitleFilter.className = "div-title-filter";
-//     divDefinePrice.className = "div-define-price";
-
-//     input.id = "filter";
-//     input.type = "range";
-//     input.min = minPrice;
-//     input.max = maxPrice;
-//     input.step = "any";
-
-//     output.id = "value-filter";
-//     output.innerText = "R$ 0,00";
-//     valueInput.innerText = "Valor: ";
-
-//     input.min = categories.forEach((element) => {
-//         let button = document.createElement("button");
-//         let titleButton = document.createElement("p");
-
-//         button.className = "categorie-button";
-//         titleButton.innerText = element;
-
-//         divButtonsFilter.append(button);
-//         button.append(titleButton);
-//     });
-
-//     return div;
-// }
-
 function renderFilterMusic(products, categories) {
+    let main = document.querySelector("main");
+
     let div = document.createElement("div");
     let divTitleFilter = document.createElement("div");
     let h2 = document.createElement("h2");
     let divButtonsFilter = document.createElement("div");
+    let divDefineFilter = document.createElement("div");
     let divDefinePrice = document.createElement("div");
     let textDefinePrice = document.createElement("p");
     let valueInput = document.createElement("p");
@@ -107,7 +58,7 @@ function renderFilterMusic(products, categories) {
     divTitleFilter.className = "div-title-filter";
     divDefinePrice.className = "div-define-price";
 
-    h2.innerText = "Filtro de Música";
+    h2.innerText = "Genero musical";
 
     input.id = "filter";
     input.type = "range";
@@ -117,24 +68,29 @@ function renderFilterMusic(products, categories) {
 
     output.id = "value-filter";
     output.innerText = "R$ 0,00";
-    valueInput.innerText = "Valor: ";
+    valueInput.innerText = "Até ";
 
+    divDefineFilter.className = "div-define-filter";
     textDefinePrice.innerText = "Definir preço";
+    textDefinePrice.className = "text-define-price";
     divDefinePrice.append(textDefinePrice, valueInput);
     valueInput.append(output);
 
-    div.append(divTitleFilter, divDefinePrice, input);
+    div.append(divTitleFilter, divDefinePrice, divDefineFilter);
     divTitleFilter.append(h2, divButtonsFilter);
-
-    categories.forEach((element) => {
+    divDefineFilter.append(divDefinePrice, input);
+    categories.map((element, index) => {
         let button = document.createElement("button");
         let titleButton = document.createElement("p");
+
+        button.setAttribute("data-id", index);
 
         button.className = "categorie-button";
         titleButton.innerText = element;
 
         button.append(titleButton);
         divButtonsFilter.append(button);
+        return button;
     });
 
     input.addEventListener("input", function () {
@@ -142,11 +98,44 @@ function renderFilterMusic(products, categories) {
     });
 
     output.innerText = formatToBRL(parseFloat(input.value));
+    function updateRangeBackground() {
+        const value = input.value;
+        const min = input.min ? input.min : 0;
+        const max = input.max ? input.max : 100;
 
-    return div;
+        const percentage = ((value - min) / (max - min)) * 100;
+        input.style.background = `linear-gradient(to right, var(--color-brand-1) ${percentage}%, var(--color-grey-5) ${percentage}%)`;
+    }
+    updateRangeBackground();
+    input.addEventListener("input", updateRangeBackground);
+
+    main.append(div);
 }
 
-// Função para formatar o valor em reais
+function filterMusic(list) {
+    let listbuttons = document.querySelectorAll(".categorie-button");
+
+    console.log(listbuttons);
+    function removeDivMusics() {
+        let divMusics = document.querySelector(".div-musics");
+
+        divMusics.remove();
+    }
+    listbuttons.forEach((element) => {
+        element.addEventListener("click", (e) => {
+            let id = Number(element.getAttribute("data-id"));
+            if (id === 0) {
+                removeDivMusics();
+                renderMusic(list);
+                return;
+            }
+            let listFiltred = products.filter((x) => x.category === id);
+            removeDivMusics();
+            renderMusic(listFiltred);
+        });
+    });
+}
+
 function formatToBRL(value) {
     return new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -154,6 +143,61 @@ function formatToBRL(value) {
     }).format(value);
 }
 
+function renderMusic(list) {
+    let main = document.querySelector("main");
+
+    let divMusics = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let ul = document.createElement("ul");
+
+    h1.innerText = "Albuns Encontrados";
+    divMusics.className = "div-musics";
+    ul.className = "list-musics";
+
+    divMusics.append(h1, ul);
+    list.forEach((element) => {
+        let priceFormated = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        }).format(element.price);
+
+        let liMusic = document.createElement("li");
+        let figure = document.createElement("figure");
+        let img = document.createElement("img");
+        let divInformations = document.createElement("div");
+        let divSinger = document.createElement("div");
+        let singerName = document.createElement("p");
+        let year = document.createElement("p");
+        let nameMusic = document.createElement("h3");
+        let divPrice = document.createElement("div");
+        let price = document.createElement("strong");
+        let buttonBuy = document.createElement("button");
+        let divAllInformations = document.createElement("div");
+
+        figure.className = "music-image";
+        img.src = element.img;
+        divInformations.className = "div-information-music";
+        divSinger.className = "div-singer";
+        singerName.innerText = element.band;
+        year.innerText = element.year;
+        nameMusic.className = "name-music";
+        nameMusic.innerText = element.title;
+        divPrice.className = "div-price-music";
+        price.innerText = priceFormated;
+        buttonBuy.innerText = "Comprar";
+        divAllInformations.className = "div-all-informationa";
+
+        liMusic.append(figure, divAllInformations);
+        divAllInformations.append(divSinger, nameMusic, divPrice);
+        figure.append(img);
+        divSinger.append(singerName, year);
+        divPrice.append(price, buttonBuy);
+        ul.append(liMusic);
+    });
+    main.append(divMusics);
+}
+
 renderHeader();
 
 renderMain(products);
+filterMusic(products);
